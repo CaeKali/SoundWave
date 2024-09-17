997a6db4f8cfffe419057719a6bb42da05d07ea3
caesar <ckalikude@gmail.com>
2024-09-17 21:25:12 +0200

    Finished Project

--- app/src/main/java/com/example/soundwave/ui/components/SongList.kt
+++ app/src/main/java/com/example/soundwave/ui/components/SongList.kt
@@ -1,8 +1,6 @@
 package com.example.soundwave.ui.components
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
